529c0cf8a2df3e3cfd7c85cdfaab3499cde818b6
Antonio Kim <antoniok9130@gmail.com>
2019-05-05 20:22:01 -0400

    Implemented variadic constructor for base module class

--- src/CML/nn/Modules.h
+++ src/CML/nn/Modules.h
@@ -1,31 +1,39 @@
 #ifndef __CML_NN_MODULES_H__
 #define __CML_NN_MODULES_H__
 
+#include <iterator>
 #include <memory>
 #include <vector>
 
 #include "Parameter.h"
 #include "../Tensor.h"
 
+#define USE_VARIADIC_CONSTRUCTOR
+
 namespace cml {
 namespace nn {
     
     class Module;
     typedef std::unique_ptr<cml::nn::Module> ModuleP;
+    typedef std::vector<ModuleP> Modules;
     
     class Module {
         protected:
             std::vector<cml::nn::Parameter> params;
-            std::vector<ModuleP> submodules;
+            Modules submodules;
         
         public:
             Module();
-            Module(std::vector<ModuleP>& submodules);
+            // Module(Modules submodules);
+            template<typename ...T>
+            Module(T&&...submodules) {
+                ModuleP mps[] = {std::move(submodules)...};
+                this->submodules = Modules{std::make_move_iterator(std::begin(mps)),
+                                     std::make_move_iterator(std::end(mps))};
+            }
         
             virtual cml::Tensor forward(const cml::Tensor&) = 0;
-            cml::Tensor operator()(const cml::Tensor& x){
-                return forward(x);
-            }
+            cml::Tensor operator()(const cml::Tensor& x){ return forward(x); }
         
             void addModule(ModuleP&);
     };
@@ -38,8 +46,8 @@ namespace nn {
     class Sequential: public Module {
         public:
             Sequential();
-            Sequential(std::vector<ModuleP>&);
-            Sequential(std::vector<ModuleP>&&);
+            template<typename ...T>
+            Sequential(T&&...submodules): Module(std::forward<T>(submodules)...) {}
 
             cml::Tensor forward(const cml::Tensor&) override;
     };
