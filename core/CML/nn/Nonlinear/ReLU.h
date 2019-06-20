ad025ca3419ca287d4c2f2d3fb37d31ab4c8f253
Antonio Kim <antoniok9130@gmail.com>
2019-06-20 19:59:10 -0400

    Moved Linear and NonLinear module definitions to header files

---
+++ core/CML/nn/Nonlinear/ReLU.h
@@ -0,0 +1,39 @@
+#ifndef __CML_NN_NONLINEAR_RELU_H__
+#define __CML_NN_NONLINEAR_RELU_H__
+
+
+#include <ostream>
+#include <string>
+
+#include "../Modules.h"
+#include "../../Tensor.h"
+#include "../../Functions/NonLinear/ReLU.h"
+
+namespace cml {
+namespace nn {
+
+    template<typename T = float>
+    class ReLU: public Module<T> {
+        /*
+        Applies the rectified linear unit function element-wise:
+        
+        ReLU(x) = max(0, x)
+        
+        Params:
+            inplace - can optionally do the operation in-place. Default: False
+        */
+        
+        bool inplace;
+        
+        public:
+            ReLU(const bool& inplace = false): inplace {inplace} {}
+
+            cml::tensor<T> forward(cml::tensor<T> x) override { return Function::ReLU::forward(x); }
+
+            std::ostream& print(std::ostream& out, const std::string& indent) override { return out << "ReLU {}"; }
+    };
+
+}
+}
+
+#endif // __CML_NN_NONLINEAR_RELU_H__
---
+++ core/CML/nn/Nonlinear/Sigmoid.h
@@ -0,0 +1,33 @@
+#ifndef __CML_NN_NONLINEAR_SIGMOID_H__
+#define __CML_NN_NONLINEAR_SIGMOID_H__
+
+#include "../Modules.h"
+#include "../../Tensor.h"
+#include "../../Functions/NonLinear/Sigmoid.h"
+
+namespace cml {
+namespace nn {
+
+    template<typename T = float>
+    class Sigmoid: public Module<T> {
+        /*
+        Applies the Sigmoid function element-wise.
+        
+        Params:
+            inplace - can optionally do the operation in-place. Default: False
+        */
+        
+        bool inplace;
+        
+        public:
+            Sigmoid(const bool& inplace = false): inplace {inplace} {}
+
+            cml::tensor<T> forward(cml::tensor<T> x) override { return Function::Sigmoid::forward(x); }
+
+            std::ostream& print(std::ostream& out, const std::string& indent) override { return out << "Sigmoid {}"; }
+    };
+
+}
+}
+
+#endif // __CML_NN_NONLINEAR_SIGMOID_H__
