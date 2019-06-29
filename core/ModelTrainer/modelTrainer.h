2e34968a348444e9bb024bd2a2eb7d00c8bc1e98
Antonio Kim <antoniok9130@gmail.com>
2019-06-29 14:25:39 -0400

    Added random shuffling of training data and labels

--- core/ModelTrainer/modelTrainer.h
+++ core/ModelTrainer/modelTrainer.h
@@ -1,12 +1,14 @@
 #ifndef __CML_MODELTRAINER_MODELTRAINER_H__
 #define __CML_MODELTRAINER_MODELTRAINER_H__
 
+#include <algorithm>
 #include <iostream>
 #include <initializer_list>
 #include <memory>
 #include <string>
 #include <utility>
 #include <vector>
+#include <Eigen/Core>
 
 #include "../CML/Tensor.h"
 #include "../CML/Dtypes.h"
@@ -22,6 +24,7 @@ namespace cml {
 
         nn::Module<T>* model = nullptr;
         DataSource<T>* data = nullptr;
+        DataSource<T>* labels = nullptr;
         std::string criterionName;
         std::unique_ptr<nn::Criterion<T>> criterion;
         std::string optimizerName;
@@ -30,17 +33,25 @@ namespace cml {
         unsigned int batchSize = 32;
         unsigned int subdivisions = 1;
         unsigned int numAccumulate = 1;
+        
+        unsigned int startingEpoch = 0;
+        unsigned int startingIterations = 0;
 
         public:
             ModelTrainer(nn::Module<T>& model, 
                          DataSource<T>& data, 
+                         DataSource<T>& labels, 
                          const std::string& criterionName, 
                          const std::string& optimizerName,
                          Kwargs kwargs = {}):
-                model{&model}, data{&data}, 
+                model{&model}, data{&data}, labels{&labels}, 
                 criterionName{criterionName}, criterion{make_criterion<T>(criterionName)}, 
                 optimizerName{optimizerName}, 
                 optimizer{make_optimizer<T>(optimizerName, model, std::forward<Kwargs>(kwargs))} {
+                
+                if (this->data->data.rows() != this->labels->data.rows()){
+                    throw "Number of data samples does not match number of labels";
+                }
                 
                 if (kwargs.count("batchSize")){
                     batchSize = (unsigned int) kwargs["batchSize"];
@@ -51,6 +62,23 @@ namespace cml {
                 if (kwargs.count("numAccumulate")){
                     numAccumulate = (unsigned int) kwargs["numAccumulate"];
                 }
+                if (kwargs.count("startingEpoch")){
+                    startingEpoch = (unsigned int) kwargs["startingEpoch"];
+                }
+                if (kwargs.count("startingIterations")){
+                    startingIterations = (unsigned int) kwargs["startingIterations"];
+                }
+            }
+            
+            void shuffle() {
+                Eigen::VectorXi indices = Eigen::VectorXi::LinSpaced(data->data.rows(), 0, data->data.rows());
+                std::random_shuffle(indices.data(), indices.data() + data->data.rows());
+                data->data = indices.asPermutation() * data->data;
+                labels->data = indices.asPermutation() * labels->data;
+            }
+            
+            void train(){
+                
             }
 
             std::ostream& print(std::ostream& out){
