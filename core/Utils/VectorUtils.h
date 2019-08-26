f8913c4cae035ed2ff266e6b377420401dadae33
Antonio Kim <antoniok9130@gmail.com>
2019-08-26 12:28:18 -0400

    Implemented Multidimensional initializer list

--- core/Utils/VectorUtils.h
+++ core/Utils/VectorUtils.h
@@ -4,6 +4,9 @@
 #include <vector>
 #include <ostream>
 
+#include "Exceptions.h"
+#include "../Numeric/Numeric.h"
+
 template <typename Iterator>
 std::ostream& print(std::ostream& out, Iterator begin, Iterator end ) {
     out << "[";
@@ -21,13 +24,58 @@ inline std::ostream& operator<<(std::ostream& out, std::initializer_list<T> v){ 
 namespace cml {
 
     template<typename T, unsigned int N>
-    struct NestedInitializerList {
-        using type = std::initializer_list<typename NestedInitializerList<T, N - 1>::type>;
+    struct MultiDimensionalInitializerList {
+        using type = std::initializer_list<typename MultiDimensionalInitializerList<T, N - 1>::type>;
     };
 
     template<typename T>
-    struct NestedInitializerList<T, 0> {
+    struct MultiDimensionalInitializerList<T, 0> {
         using type = T;
+    };
+    
+    template<typename T, unsigned int N>
+    using nd_array = typename MultiDimensionalInitializerList<T, N>::type;
+    
+    template<typename T, size_t... dims>
+    struct MultiDimensionalInitializerListProcessor;
+    
+    template<typename T, size_t first, size_t... rest>
+    struct MultiDimensionalInitializerListProcessor<T, first, rest...> {
+    
+        static constexpr size_t R = cml::numeric::product<rest...>();
+        static constexpr size_t S = first*R;
+        static constexpr size_t N = 1+(sizeof...(rest));
+    
+        static std::array<T, S> getArray(nd_array<T, N> a){
+            auto arr = std::array<T, S>();
+            process(std::forward<nd_array<T, N>>(a), arr.data());
+            return arr;
+        }
+    
+        static void process(nd_array<T, N> a, T* data){
+            if (a.size() > first){
+                throw CMLException("Multidimensional Initializer list size is greater than first");
+            }
+            int i = 0;
+            for (auto e : a){
+                MultiDimensionalInitializerListProcessor<T, rest...>::process(e, data+R*(i++));
+            }
+        }
+        
+    };
+    
+    template<typename T, size_t last>
+    struct MultiDimensionalInitializerListProcessor<T, last> {
+    
+        static void process(nd_array<T, 1> a, T* data){
+            if (a.size() > last){
+                throw CMLException("Multidimensional Initializer list size is greater than last");
+            }
+            for (auto e : a){
+                *(data++) = e;
+            }
+        }
+        
     };
 
 }
